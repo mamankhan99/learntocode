7fd9953c6f0ce7c2a664f203c0ac57a2e56a2562
muaazkareem <muaazkareem007@gmail.com>
2020-12-22 07:51:27 -0800

    6th program

---
+++ days_alive.py.py
@@ -0,0 +1,55 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> hours = 9
+>>> hours = hours + 1
+>>> 
+>>> hours
+10
+>>> hours = hours - 1
+>>> hours
+9
+>>> hours = hours + 1
+>>> hours
+10
+>>> hours = hours * 1
+>>> hours
+10
+>>> hours = hours * 2
+>>> hours
+20
+>>> hours = 9
+>>> hours = 9 + 1
+>>> hours = hours * 2
+>>> print(hours)
+20
+>>> minutes = 30
+>>> minutes = minutes + 1
+>>> second = minutes * 60
+>>> second
+1860
+>>> minutes
+31
+>>> age = 15
+>>> print(age * 365)
+5475
+>>> age = 100
+>>> print (age * 365)
+36500
+>>> age = 15
+>>> print(age * 365)
+5475
+>>> days_per year = 365
+SyntaxError: invalid syntax
+>>> days_per_year = 365
+>>> days_alive = age * days_per_year
+>>> days_alive
+5475
+>>> print
+<built-in function print>
+(
+>>>  print(days_alive)
+ 
+SyntaxError: unexpected indent
+>>> print(days_alive)
+5475
+>>> 
