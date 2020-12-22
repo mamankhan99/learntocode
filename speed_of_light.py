b497b5ce71f0ada825d65bbe59ecd601312c68f2
muaazkareem <muaazkareem007@gmail.com>
2020-12-22 06:58:32 -0800

    4th program

---
+++ speed_of_light.py
@@ -0,0 +1,14 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(299792458 * 100)
+29979245800
+>>> print(299792458 * 100 * 1/1000000000)
+29.9792458
+>>> print(299792458 * 100)
+29979245800
+>>> speed_of_light = 299792458
+>>> print(speed_of_light)
+299792458
+>>> print(speed_of_light * 365 *24 * 60 * 60)
+9454254955488000
+>>> 
