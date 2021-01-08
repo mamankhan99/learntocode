4f902726602fab073a6054291d372e5e78246ebd
muaazkareem <muaazkareem007@gmail.com>
2021-01-08 02:51:48 -0800

    noob program

---
+++ Your_givien_work.py
@@ -0,0 +1,21 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> "python"
+'python'
+>>> 'python'[-3]
+'h'
+>>> 'python'[0]
+'p'
+>>> 'python'[1]
+'y'
+>>> 'python'[2]
+'t'
+>>> 'python'[3]
+'h'
+>>> 'python'[4]
+'o'
+>>> 'python'[5]
+'n'
+>>> 'python'[:]
+'python'
+>>> 
